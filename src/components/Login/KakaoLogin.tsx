402909f5fce0abf8ef4ce1fd54a764f9352222c3
cherry.a <cherry.a@kakaocorp.com>
2025-11-15 22:12:33 +0900

    add env valuable on the code

--- src/components/Login/KakaoLogin.tsx
+++ src/components/Login/KakaoLogin.tsx
@@ -1,9 +1,10 @@
 import styles from './KakaoLogin.module.scss'
 
+const REST_API_KEY = import.meta.env.PUBLIC_KAKAO_REST_API_KEY || '4ea5e31b7c9fd5131c30f91acac4abb0'
+const KAKAO_REDIRECT_URI = import.meta.env.PUBLIC_KAKAO_REDIRECT_URI || 'https://time2gather.com/login/oauth2/code/kakao'
+
 export default function KakaoLogin () {
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${
-    import.meta.env.PUBLIC_KAKAO_REST_API_KEY
-  }&redirect_uri=${import.meta.env.PUBLIC_KAKAO_REDIRECT}`
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}`
 
   return (
     <button className={styles.button} onClick={() => window.location.replace(`${KAKAO_AUTH_URL}`)}>
