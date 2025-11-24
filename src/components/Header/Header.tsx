8dc7df427984665ae173c082bca5d608485ecf87
dev_hee <kheeyaa888@gmail.com>
2025-11-24 23:54:21 +0900

    improve header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,43 @@
 import { PiHamburger } from "react-icons/pi";
 import { HiChevronLeft } from "react-icons/hi";
 import styles from "./Header.module.scss";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-use";
 
 export default function Header() {
+  const location = useLocation()
+  const [hasHistory, setHasHistory] = useState(false)
+
+  useEffect(() => {
+    if (location) {
+      const pathname = location.pathname || window.location.pathname;
+      const isHomePage = pathname === '/' || pathname === '';
+      setHasHistory(!isHomePage);
+    }
+  }, [location]);
+
+  const handleBack = () => {
+    window.history.back();
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.left}>
-        <button id="backButton" className={styles.backButton}>
-          <HiChevronLeft size={24} />
-        </button>
-      </div>
-      <a href="/" className={styles.title}>Time2Gather</a>
+    <header className={`${styles.header} ${!hasHistory ? styles.noBackButton : ''}`}>
+      {hasHistory && (
+        <div className={styles.left}>
+          <button 
+            className={styles.backButton}
+            onClick={handleBack}
+          >
+            <HiChevronLeft size={24} />
+          </button>
+        </div>
+      )}
+      <a 
+        href="/" 
+        className={`${styles.title} ${hasHistory ? styles.center : styles.left}`}
+      >
+        Time2Gather
+      </a>
       <div className={styles.right}>
         <button className={styles.menuButton}>
           <PiHamburger size={24} />
