4ac4e2316125fd342ed88dc01c3ade36bc9343a6
cherry.a <cherry.a@kakaocorp.com>
2025-11-24 13:57:46 +0900

    send optional redirectUrl param in api request

--- src/components/Login/Oauth.tsx
+++ src/components/Login/Oauth.tsx
@@ -8,8 +8,18 @@ export default function Oauth () {
   useAsync(async () => {
     if (!code) return
 
-    const response = await auth.oauth.$provider.post('kakao', code)
-    console.log({response})
+    const response = await auth.oauth.$provider.post('kakao', {
+      authorizationCode: code,
+      ...(
+        import.meta.env.DEV
+        ?
+          {
+            redirectUrl: 'http://localhost:3000/login/oauth2/code/kakao'
+          }
+        : null
+      )
+    })
+
     navigate('/meetings/create')
   }, [code])
 
