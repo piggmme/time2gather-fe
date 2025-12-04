063f9d7b2e95e2b30e8ec46cd741a846e42e060e
cherry.a <cherry.a@kakaocorp.com>
2025-12-04 19:21:51 +0900

    implement time range select feature

--- src/components/SelectPage/SelectPage.tsx
+++ src/components/SelectPage/SelectPage.tsx
@@ -4,11 +4,11 @@ import { useTranslation } from "../../hooks/useTranslation";
 import styles from './SelectPage.module.scss';
 import { useEffect, useRef, useState } from "react";
 import Button from "../Button/Button";
-import { meetings } from "../../services/meetings";
+import { meetings, type get_meetings_$meetingCode_response } from "../../services/meetings";
 
 export default function SelectPage(
-  { meetingCode, dates, availableTimes }:
-  { meetingCode: string, dates: string[], availableTimes: string[] }
+  { meetingCode, data }:
+  { meetingCode: string, data: get_meetings_$meetingCode_response['data'] }
 ) {
   const { t } = useTranslation();
   const containerRef = useRef<HTMLDivElement>(null);
@@ -23,7 +23,11 @@ export default function SelectPage(
     }
   }, []);
 
+  console.log({data})
+
+  const dates = Object.keys(data.meeting.availableDates);
   const dateList = dates.map((date) => dayjs(date)).sort((a, b) => a.diff(b));
+  const availableTimes = Object.values(data.meeting.availableDates)[0] || [];
 
   return (
     <>
@@ -44,10 +48,11 @@ export default function SelectPage(
         buttonType="primary"
         disabled={Object.entries(selections).every(([_, times]) => times.length === 0)}
         onClick={async () => {
+          // selections 에서 빈배열인 날짜는 제거
+          const filteredSelections = Object.fromEntries(Object.entries(selections).filter(([_, times]) => times.length > 0));
           const response = await meetings.$meetingCode.selections.put(meetingCode, {
-            selections
+            selections: filteredSelections
           });
-          console.log({response})
         }}
       >
         {t('common.submit')}
